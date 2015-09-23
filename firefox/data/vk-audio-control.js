21dca4cbd91776bd029ba45cfacaf29ed1a9ffa3
kusov <kusov@wiley.com>
2015-09-23 16:20:52 +0300

    Update to jpm instead of cfx

--- firefox/data/vk-audio-control.js
+++ firefox/data/vk-audio-control.js
@@ -1,10 +1,20 @@
-self.port.on('player', function(code) {
-  var p = unsafeWindow.audioPlayer;
-  if (code == 0) code = p.player.paused() ? 1 : 2;
-  switch (code) {
-    case 1: p.playTrack();  break;
-    case 2: p.pauseTrack(); break;
-    case 3: p.prevTrack();  break;
-    case 4: p.nextTrack();  break;
-  }
+self.port.on('player', function (command) {
+    var p = unsafeWindow.audioPlayer;
+    var response = p.player.paused() ? 'paused' : 'playing';
+    if (command == 'toggle') command = p.player.paused() ? 'play' : 'pause';
+    switch (command) {
+        case 'play':
+            p.playTrack();
+            break;
+        case 'pause':
+            p.pauseTrack();
+            break;
+        case 'prev':
+            p.prevTrack();
+            break;
+        case 'next':
+            p.nextTrack();
+            break;
+    }
+    self.port.emit('response', response)
 });
--- firefox/index.js
+++ firefox/index.js
@@ -3,36 +3,45 @@ const PORT = 4851;
 var serverSocket;
 
 exports.main = function (options, callbacks) {
-  var page = require('sdk/page-mod').PageMod({
-    include: /(https?:\/\/)?vk.com\/.*/,
-    contentScriptFile: './vk-audio-control.js'
-  });
+    var page = require('sdk/page-mod').PageMod({
+        include: /(https?:\/\/)?vk.com\/.*/,
+        contentScriptFile: './vk-audio-control.js'
+    });
 
-  const {Cc,Ci} = require('chrome');
-  serverSocket = Cc['@mozilla.org/network/server-socket;1'].createInstance(Ci.nsIServerSocket);
-  serverSocket.init(PORT, true, -1);
-  serverSocket.asyncListen({
-    onSocketAccepted: function(server, transport) {
-      var inputStream = transport.openInputStream(0, 0, 0);
-      var binaryInput = Cc['@mozilla.org/binaryinputstream;1'].createInstance(Ci.nsIBinaryInputStream);
-      binaryInput.setInputStream(inputStream);
+    const {Cc,Ci} = require('chrome');
+    serverSocket = Cc['@mozilla.org/network/server-socket;1'].createInstance(Ci.nsIServerSocket);
+    serverSocket.init(PORT, true, -1);
+    serverSocket.asyncListen({
+        onSocketAccepted: function (server, transport) {
+            var inputStream = transport.openInputStream(0, 0, 0);
+            var binaryInput = Cc['@mozilla.org/binaryinputstream;1'].createInstance(Ci.nsIBinaryInputStream);
+            binaryInput.setInputStream(inputStream);
 
-      var wait = function() { inputStream.asyncWait(callback, 0, 0, Cc['@mozilla.org/thread-manager;1'].getService().mainThread) };
+            var outputStream = transport.openOutputStream(0, 0, 0);
+            var binaryOutput = Cc['@mozilla.org/binaryoutputstream;1'].createInstance(Ci.nsIBinaryOutputStream);
+            binaryOutput.setOutputStream(outputStream);
 
-      var callback = {
-        onInputStreamReady: function(stream) {
-          code = binaryInput.read8();
-          console.exception(code);
-          page.port.emit('player', code);
-          console.exception('ok');
-          wait();
+            function wait() {inputStream.asyncWait(callback, 0, 0, Cc['@mozilla.org/thread-manager;1'].getService().mainThread)}
+
+            var callback = {onInputStreamReady: function (stream) {
+                try {
+                    var action = binaryInput.readCString();
+                    page.port.emit('player', action)
+                } catch (e) {
+                    console.log(e)
+                }
+            }};
+
+            page.port.on('response', function(response) {
+                binaryOutput.writeUtf8Z(response)
+                wait();
+            });
+
+            wait();
         }
-      };
-      wait();
-    }
-  });
+    });
 };
 
 exports.onUnload = function (reason) {
-  serverSocket.close();
+    serverSocket.close();
 };
